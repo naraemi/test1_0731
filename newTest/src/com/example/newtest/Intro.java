9a318fb1747e9437eca2aec142b6f3909674044d
naraemi <SAMSUNG@NARAE-PC>
2014-07-31 12:35:52 +0900

    test해볼게용

---
+++ newTest/src/com/example/newtest/Intro.java
@@ -0,0 +1,32 @@
+package com.example.newtest;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Intro extends Activity {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.intro);
+		
+		
+		new Thread(new Runnable() {
+			public void run() {
+				// TODO Auto-generated method stub
+				try {					
+					Thread.sleep(3000);
+					} catch (Throwable ex) {
+					ex.printStackTrace();
+					}
+				Intent intent_intro = new Intent(Intro.this, MainActivity.class);
+				startActivity(intent_intro);
+				finish();
+			}
+			}).start();
+	}
+	  
+}

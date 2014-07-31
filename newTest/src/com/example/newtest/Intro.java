c871e2571f2e3b9c1e8e259244b273a836dad197
naraemi <SAMSUNG@NARAE-PC>
2014-07-31 12:50:57 +0900

    테스트라고썼어

--- newTest/src/com/example/newtest/Intro.java
+++ newTest/src/com/example/newtest/Intro.java
@@ -13,7 +13,7 @@ public class Intro extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.intro);
 		
-		String test="";
+		String test="Å×½ºÆ®";
 		new Thread(new Runnable() {
 			public void run() {
 				// TODO Auto-generated method stub
